c78a7fe2c0f8bc69654ae7bf76693bd311f1fa8c
alxndro373 <josealex2004@hotmail.com>
2025-10-03 17:53:32 -0600

    views of categorias/productos

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 from routes.usuarios import usuarios_bp
+from routes.categorias import categorias_bp
+from routes.productos import productos_bp
 
 app = Flask(__name__)
 
 #Blueprints
 app.register_blueprint(usuarios_bp, url_prefix="/usuarios")
+app.register_blueprint(categorias_bp, url_prefix="/categorias")
+app.register_blueprint(productos_bp, url_prefix="/productos")
 
 @app.route("/")
 def home():
